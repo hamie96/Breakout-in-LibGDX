3a2ae674aedd8305128c9a25573fca59f7126f93
hamie96 <hamie96@gmail.com>
2020-01-07 00:32:37 -0500

    Started Breakout clone

---
+++ desktop/src/com/breakout/clone/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.breakout.clone.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.breakout.clone.Breakout;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Breakout(), config);
+	}
+}
